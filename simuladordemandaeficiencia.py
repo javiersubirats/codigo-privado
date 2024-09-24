e27246881b83bbedcf54c61f8def72e2d7f19673
javiersubirats <javier.subirats@gmail.com>
2024-09-24 08:57:52 +0200

    Update simuladordemandaeficiencia.py

--- simuladordemandaeficiencia.py
+++ simuladordemandaeficiencia.py
@@ -85,13 +85,7 @@ if st.button("Generar Gráficos"):
         it_difference = it[:-1] - it[1:]
         it_difference[it_difference < 0] = 0  
         plt.plot(prices_extended[:-1], it_difference, label=f'α = {alpha}, p0 = {p0}')  # Añadir p0 en la leyenda
-        
-        max_it_index = np.argmax(it)
-        max_it_price = prices_extended[max_it_index]
-        plt.axvline(x=max_it_price, linestyle='--', color='red')
-        plt.text(max_it_price, it[max_it_index], f'Max IT\np={max_it_price:.1f}', 
-                 horizontalalignment='left', fontsize=8, color='red')
-        
+               
     plt.title('Ingreso Marginal IT(p) - IT(p+1) (solo positivos)', fontsize=14)
     plt.xlabel('Precio (p)', fontsize=12)
     plt.ylabel('Ingreso Marginal', fontsize=12)
