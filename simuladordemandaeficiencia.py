6cd1a3c7533c0a13602f3c4c296091851670b714
javiersubirats <javier.subirats@gmail.com>
2024-09-24 09:11:00 +0200

    Update simuladordemandaeficiencia.py

--- simuladordemandaeficiencia.py
+++ simuladordemandaeficiencia.py
@@ -55,6 +55,8 @@ if st.button("Generar Gráficos"):
     plt.xlabel('Precio (p)', fontsize=12)
     plt.ylabel('Demanda D(p)', fontsize=12)
     plt.axvline(x=p0, color='gray', linestyle='--', label=f'Precio p0 = {p0}')
+    plt.text(max_it_price, demands[max_it_index], f'D(p)\n{demands[max_it_index]:.1f}', 
+                 horizontalalignment='left', fontsize=8, color='grey')
     plt.legend()
     plt.grid(True)
     
